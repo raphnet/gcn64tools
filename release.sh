84b862b2554e01beab6c1db491ba0e98d9fc0c7b
Raphael Assenat <raph@raphnet.net>
2016-02-16 14:44:45 -0500

    run git tag when releasing

--- release.sh
+++ release.sh
@@ -10,6 +10,8 @@ if [ $# -ne 2 ]; then
 	echo
 	echo "ex: './release 1.0' will produce $PREFIX-1.0.tar.gz in releasedir out of git HEAD,"
 	echo "and place it in the release directory."
+	echo
+	echo "It will also create a tag named v1.0"
 	exit
 fi
 
@@ -17,6 +19,7 @@ VERSION=$1
 RELEASEDIR=$2
 DIRNAME=$PREFIX-$VERSION
 FILENAME=$PREFIX-$VERSION.tar.gz
+TAG=v$VERSION
 
 echo "Version: $VERSION"
 echo "Filename: $FILENAME"
@@ -31,6 +34,7 @@ if [ -f $RELEASEDIR/$FILENAME ]; then
 	exit 1
 fi
 
+git tag $TAG
 git archive --format=tar --prefix=$DIRNAME/ HEAD | gzip > $RELEASEDIR/$FILENAME
 
 #cd $RELEASEDIR
