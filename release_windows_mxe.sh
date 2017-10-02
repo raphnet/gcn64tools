b8e6760f0fea47213eadc9c52fbf9ff37735fb1f
Raphael Assenat <raph@raphnet.net>
2017-10-01 23:55:08 -0400

    Copy glib schemas when building windows installer

--- release_windows_mxe.sh
+++ release_windows_mxe.sh
@@ -4,6 +4,7 @@ MXE_ROOT=$HOME/mxe
 VERSION_INC=./src/version.inc
 VERSION=`cat $VERSION_INC | cut -d '=' -f 2` # VERSION=x.x.x
 TMPDIR="./tmp"
+GLIB_SCHEMAS=$MXE_ROOT/usr/i686-w64-mingw32.static/share/glib-2.0/schemas
 
 if [[ $# > 0 ]]; then
 	VERSION=$1
@@ -26,6 +27,10 @@ echo "Checking for dependencies..."
 command -v makensis || errorexit 1 "makensis not found"
 command -v $QMAKE || errorexit 1 "qmake not found"
 
+if [ ! -d $GLIB_SCHEMAS ]; then
+	exit GLIB Schemas not found at $GLIB_SCHEMAS
+fi
+
 echo $MXE_ROOT
 echo $VERSION
 
@@ -35,6 +40,8 @@ rm -rf $TMPDIR
 mkdir $TMPDIR
 cp src/*.exe $TMPDIR
 cp -r windows_build_resources/* $TMPDIR
+mkdir -p $TMPDIR/share/glib-2.0
+cp $GLIB_SCHEMAS -r $TMPDIR/share/glib-2.0
 cp LICENSE $TMPDIR/LICENSE.txt
 cp README.md $TMPDIR
 cp README.md $TMPDIR/readme.txt
